20a4455fff007cb5e12499c19c6256988bf757ce
kamilic <wangjin@mama.cn>
2023-06-18 23:46:57 +0800

    feat: complete partial testcase for LockUtil

--- src/main/java/edu/berkeley/cs186/database/concurrency/LockUtil.java
+++ src/main/java/edu/berkeley/cs186/database/concurrency/LockUtil.java
@@ -2,6 +2,8 @@ package edu.berkeley.cs186.database.concurrency;
 
 import edu.berkeley.cs186.database.TransactionContext;
 
+import java.util.*;
+
 /**
  * LockUtil is a declarative layer which simplifies multigranularity lock
  * acquisition for the user (you, in the last task of Part 2). Generally
@@ -12,9 +14,9 @@ public class LockUtil {
     /**
      * Ensure that the current transaction can perform actions requiring
      * `requestType` on `lockContext`.
-     *
+     * <p>
      * `requestType` is guaranteed to be one of: S, X, NL.
-     *
+     * <p>
      * This method should promote/escalate/acquire as needed, but should only
      * grant the least permissive set of locks needed. We recommend that you
      * think about what to do in each of the following cases:
@@ -22,9 +24,9 @@ public class LockUtil {
      * - The current lock type is IX and the requested lock is S
      * - The current lock type is an intent lock
      * - None of the above: In this case, consider what values the explicit
-     *   lock type can be, and think about how ancestor looks will need to be
-     *   acquired or changed.
-     *
+     * lock type can be, and think about how ancestor looks will need to be
+     * acquired or changed.
+     * <p>
      * You may find it useful to create a helper method that ensures you have
      * the appropriate locks on all ancestors.
      */
@@ -41,8 +43,43 @@ public class LockUtil {
         LockType effectiveLockType = lockContext.getEffectiveLockType(transaction);
         LockType explicitLockType = lockContext.getExplicitLockType(transaction);
 
+        // same lock type should return immediately
+        if (requestType.equals(explicitLockType)) {
+            return;
+        }
+
+        // The current lock type can effectively substitute the requested type
+        if (requestType.equals(LockType.NL)) {
+            lockContext.release(transaction);
+            return;
+        }
+
+        if (explicitLockType.equals(LockType.NL) && effectiveLockType.equals(LockType.NL)) {
+            LockUtil.ensureParentContext(lockContext.parent, requestType);
+            lockContext.acquire(transaction, requestType);
+        }
+
+        if (explicitLockType.equals(LockType.IS) && requestType.equals(LockType.S)) {
+            lockContext.escalate(transaction);
+        }
         // TODO(proj4_part2): implement
         return;
+    }
+
+    static public void ensureParentContext(LockContext parentContext, LockType childLock) {
+        LockContext currentLockContext = parentContext;
+        Stack<LockContext> allParentContexts = new Stack<>();
+        LockType parentLock = LockType.parentLock(childLock);
+
+        while (currentLockContext != null) {
+            allParentContexts.push(currentLockContext);
+            currentLockContext = currentLockContext.parent;
+        }
+
+        while (!allParentContexts.empty()) {
+            currentLockContext = allParentContexts.pop();
+            currentLockContext.acquire(TransactionContext.getTransaction(), parentLock);
+        }
     }
 
     // TODO(proj4_part2) add any helper methods you want
